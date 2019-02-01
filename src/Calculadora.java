53de27b8bb38ed358853fb091d304dc5e73ba99a
KristenBrandt <kabrandtf@gmail.com>
2019-01-31 23:27:52 -0600

    Main

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,3 +1,58 @@
-public class Calculadora {
-    
+import java.util.ArrayList;
+
+public class Calculadora  implements iCalculadora, iPila<String>{
+
+    //Sirve para ver si el array list tiene algo o es nulo//
+    public boolean tienealgo = true;
+    ArrayList<String> ar = new ArrayList<>();
+
+    @Override
+    public double sumar(double a, double b) {
+        return a+b;
+    }
+
+    @Override
+    public double restar(double a, double b) {
+        return a-b;
+    }
+
+    @Override
+    public double multiplicar(double a, double b) {
+        return a*b;
+    }
+
+    @Override
+    public double dividir(double a, double b) {
+        return a/b;
+    }
+
+    //FALTA
+    @Override
+    public void push(String item) {
+
+    }
+
+    //FALTA
+    @Override
+    public String pop() {
+        return null;
+    }
+
+    //FALTA
+    @Override
+    public String peek() {
+        return null;
+    }
+
+    //FALTA
+    @Override
+    public boolean empty() {
+        return false;
+    }
+
+    //FAlTA
+    @Override
+    public int size() {
+        return 0;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
-	public class Main {
 
-		/**
-		 * @param args
-		 */
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
+	static String encendermenu = "MENÚ: \n" +
+			"\t1. Encender la calculadora. \n" +
+			"\t2. Salir.";
 
-		}
+	static String encendidomenu = "MENÚ: \n" +
+			"\t1. Ingresar la direccion del archivo con la ecuacion que quiere resolver:\n" +
+			"\t2. La direccion del archivo tiene que ser en formato C:\\\\\\\\Users\\\\\\\\try\\\\\\\\Desktop\\\\\\\\intento.txt\\\\\\\n" +
+			"\t3. Salir.";
+
+
+	public static void main(String[] args) {
+		iCalculadora calculadora = new Calculadora();
+		Scanner input = new Scanner(System.in);
+		boolean continuar = true;
+		boolean seguir = true;
+
+		do {
+			System.out.print(encendermenu);
+			int encender = input.nextInt();
+
+			switch (encender) {
+				case 1:
+					System.out.print(encendidomenu);
+					String archivo = input.nextLine();
+					if (!archivo.equals("0")){
+						// TODO: 2019-01-31
+						
+					}
+
+					break;
+
+
+				case 2:
+					System.out.println("Saliendo del programa...");
+					seguir = false;
+					break;
+			}
+
+		}while (seguir);
 
 	}
-
 }
+
+
